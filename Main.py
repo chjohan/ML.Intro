8a6ae71b8e559d9289687914ca80477be4f5a7c7
Christian Johansen <c.johansen@inmeta.no>
2018-05-20 15:17:18 +0200

    added some plots

--- Main.py
+++ Main.py
@@ -14,12 +14,30 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
 # Load dataset
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+path = "./iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-dataset = pandas.read_csv(url, names=names)
+dataset = pandas.read_csv(path, names=names)
 
 #shape
 print(dataset.shape)
 
 #head
 print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('class').size())
+
+# box and whisker plots
+#dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+#plt.show()
+
+# histograms
+#dataset.hist()
+#plt.show()
+
+# scatter plot matrix
+scatter_matrix(dataset)
+plt.show()
